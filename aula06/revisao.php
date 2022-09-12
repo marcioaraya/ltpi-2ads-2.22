066e77d224babaead12ed3785e7831f33fda767b
Marcio Araya <marcio.araya@df.senac.br>
2022-09-12 09:06:15 -0300

    aula 06 revisao foreach

---
+++ aula06/revisao.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Revisão Foreach</h1>
+
+    <p>Parte 1: exibir os dados do vetor abaixo
+         como uma tabela HTML</p>
+
+   
+    <?php
+
+        $aluno[0]["nome"] = "João";
+        $aluno[0]["nota1"] = 5.0;
+        $aluno[0]["nota2"] = 6.0;
+        $aluno[0]["sexo"] = "M";
+        $aluno[0]["frequencia"] = 0.80;
+
+        $aluno[1]["nome"] = "Maria";
+        $aluno[1]["nota1"] = 7.0;
+        $aluno[1]["nota2"] = 8.5;
+        $aluno[1]["sexo"] = "F";
+        $aluno[1]["frequencia"] = 0.9;
+
+        $aluno[2]["nome"] = "Rita";
+        $aluno[2]["nota1"] = 6.0;
+        $aluno[2]["nota2"] = 7.0;
+        $aluno[2]["sexo"] = "F";
+        $aluno[2]["frequencia"] = 0.60;
+
+        $aluno[3]["nome"] = "Manuel";
+        $aluno[3]["nota1"] = 8.0;
+        $aluno[3]["nota2"] = 6.0;
+        $aluno[3]["sexo"] = "M";
+        $aluno[3]["frequencia"] = 1.00;
+
+
+
+    ?>
+</body>
+</html>
